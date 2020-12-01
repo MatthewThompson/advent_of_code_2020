7b44aaa7ea44ca4ddf9a7f9c2b0f10012ec31ad8
Matthew Thompson <mat@matthewjthompson.co.uk>
2020-12-01 19:39:42 +0000

    01 part 2

--- src/bin/aoc01/main.rs
+++ src/bin/aoc01/main.rs
@@ -26,10 +26,11 @@ fn parse_input(input: std::string::String) -> Vec<i64> {
 fn main() {
     let values = parse_input(get_input());
 
-    part_one(values);
+    part_one(&values);
+    part_two(&values);
 }
 
-fn part_one(values: Vec<i64>) -> i64 {
+fn part_one(values: &Vec<i64>) -> i64 {
 
     let (a, b) = find_sum_parts(values, 2020);
     let solution = a * b;
@@ -39,9 +40,9 @@ fn part_one(values: Vec<i64>) -> i64 {
     solution
 }
 
-fn find_sum_parts(values: Vec<i64>, total: i64) -> (i64, i64) {
+fn find_sum_parts(values: &Vec<i64>, total: i64) -> (i64, i64) {
 
-    let mut sorted = values;
+    let mut sorted = values.clone();
     sorted.sort_unstable();
 
     let mut low_index = 0;
@@ -63,10 +64,58 @@ fn find_sum_parts(values: Vec<i64>, total: i64) -> (i64, i64) {
     panic!("No solution.");
 }
 
+fn part_two(values: &Vec<i64>) -> i64 {
+
+    let (a, b, c) = find_3_sum_parts(values, 2020);
+    let solution = a * b * c;
+
+    println!("The solution to part 2 is {} ({} * {} * {}).", solution, a, b, c);
+
+    solution
+}
+
+fn find_3_sum_parts(values: &Vec<i64>, total: i64) -> (i64, i64, i64) {
+
+    let mut sorted = values.clone();
+    sorted.sort_unstable();
+
+    let mut middle_index = 1;
+    let mut high_index = sorted.len() - 1;
+
+    while middle_index < high_index {
+
+        let middle = sorted[middle_index];
+
+        if sorted[high_index] + middle > total {
+            high_index -= 1;
+        }
+        else {
+
+            let mut low_index = 0;
+
+            while low_index < middle_index && middle_index < high_index {
+
+                let low = sorted[low_index];
+                let high = sorted[high_index];
+
+                match low + middle + high {
+                    sum if sum < total => { low_index += 1; }
+                    sum if sum > total => { high_index -= 1; }
+                    _ => return (low, middle, high),
+                }
+            }
+
+            middle_index += 1;
+        }
+    }
+
+    panic!("No solution.");
+}
 
 #[cfg(test)]
 mod tests {
     use super::part_one;
+    use super::part_two;
 
     #[test]
     fn test_part_one() {
@@ -79,6 +128,20 @@ mod tests {
             1456,
         ];
 
-        assert_eq!(part_one(example_values), 514579);
+        assert_eq!(part_one(&example_values), 514579);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let example_values: Vec<i64> = vec![
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456,
+        ];
+
+        assert_eq!(part_two(&example_values), 241861950);
     }
 }
