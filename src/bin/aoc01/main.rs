b7bee4e18fb28c8e9a3a1a1891344d00587ef4bd
Matthew Thompson <mat@matthewjthompson.co.uk>
2020-12-01 14:57:41 +0000

    01 part 1

--- src/bin/aoc01/main.rs
+++ src/bin/aoc01/main.rs
@@ -1,3 +1,61 @@
+use std::env;
+use std::fs;
+
+fn default_input_path() -> std::path::PathBuf {
+    let mut input_path = env::current_dir().unwrap();
+    input_path.push("input.txt");
+
+    input_path
+}
+
+fn get_input() -> std::string::String {
+
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => fs::read_to_string(default_input_path()).expect("Error reading input file"),
+        _ => fs::read_to_string(&args[1]).expect("Error reading input file")
+    }
+}
+
+fn parse_input(input: std::string::String) -> Vec<i64> {
+    input.split_ascii_whitespace()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect()
+}
+
 fn main() {
-    println!("Waiting for Dec1...");
+    let mut values: Vec<i64> = parse_input(get_input());
+
+    part_one(&mut values);
+}
+
+fn part_one(values: &mut Vec<i64>) {
+
+    let (a, b) = find_sum_parts(values, 2020);
+    let solution = a * b;
+
+    println!("The solution to part 1 is {} ({} * {}).", solution, a, b);
+}
+
+fn find_sum_parts(values: &mut Vec<i64>, total: i64) -> (i64, i64) {
+
+    values.sort_unstable();
+
+    let mut low_index = 0;
+    let mut high_index = values.len() - 1;
+
+    while low_index < high_index {
+
+        let low = values[low_index];
+        let high = values[high_index];
+        let sum = low + high;
+
+        match sum {
+            s if s < total => { low_index += 1; }
+            s if s > total => { high_index -= 1; }
+            _ => return (low, high),
+        }
+    }
+
+    panic!("No solution.");
 }
