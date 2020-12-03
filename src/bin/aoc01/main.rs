f2dd3c09429543880410fc9c734e31985186228f
Matthew Thompson <mat@matthewjthompson.co.uk>
2020-12-03 20:36:55 +0000

    add timing information

--- src/bin/aoc01/main.rs
+++ src/bin/aoc01/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::time::SystemTime;
 
 fn default_input_path() -> std::path::PathBuf {
     let mut input_path = env::current_dir().unwrap();
@@ -24,10 +25,24 @@ fn parse_input(input: std::string::String) -> Vec<i64> {
 }
 
 fn main() {
+    let start_time = SystemTime::now();
     let values = parse_input(get_input());
 
-    part_one(&values);
-    part_two(&values);
+    let setup_time = SystemTime::now();
+    let p1 = part_one(&values);
+    let part_1_time = SystemTime::now();
+    let p2 = part_two(&values);
+    let part_2_time = SystemTime::now();
+
+    println!("The solution for part one is: {}", p1);
+    println!("The solution for part two is: {}", p2);
+    println!();
+
+    println!("Time breakdowns:");
+    println!("Setup: {:?}", setup_time.duration_since(start_time).unwrap());
+    println!("Part 1: {:?}", part_1_time.duration_since(setup_time).unwrap());
+    println!("Part 2: {:?}", part_2_time.duration_since(part_1_time).unwrap());
+    println!("Total: {:?}", part_2_time.duration_since(start_time).unwrap());
 }
 
 fn part_one(values: &Vec<i64>) -> i64 {
--- src/bin/aoc03/main.rs
+++ src/bin/aoc03/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::time::SystemTime;
 
 fn default_input_path() -> std::path::PathBuf {
     let mut input_path = env::current_dir().unwrap();
@@ -64,10 +65,24 @@ fn count_trees_on_slope(map: &TobogganMap, direction: Slope) -> usize {
 }
 
 fn main() {
+    let start_time = SystemTime::now();
     let map = parse_input(get_input());
 
-    println!("The solution for part one is: {}", part_one(&map));
-    println!("The solution for part two is: {}", part_two(&map));
+    let setup_time = SystemTime::now();
+    let p1 = part_one(&map);
+    let part_1_time = SystemTime::now();
+    let p2 = part_two(&map);
+    let part_2_time = SystemTime::now();
+
+    println!("The solution for part one is: {}", p1);
+    println!("The solution for part two is: {}", p2);
+    println!();
+
+    println!("Time breakdowns:");
+    println!("Setup: {:?}", setup_time.duration_since(start_time).unwrap());
+    println!("Part 1: {:?}", part_1_time.duration_since(setup_time).unwrap());
+    println!("Part 2: {:?}", part_2_time.duration_since(part_1_time).unwrap());
+    println!("Total: {:?}", part_2_time.duration_since(start_time).unwrap());
 }
 
 fn part_one(map: &TobogganMap) -> usize {
